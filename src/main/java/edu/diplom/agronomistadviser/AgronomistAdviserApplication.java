e88c1e7704ea2ef0d8432501fbd09fb6740d52cc
SerhiiMazur <ecm.mazur@gmail.com>
2024-09-09 11:00:05 +0300

    Initial commit

---
+++ src/main/java/edu/diplom/agronomistadviser/AgronomistAdviserApplication.java
@@ -0,0 +1,13 @@
+package edu.diplom.agronomistadviser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgronomistAdviserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgronomistAdviserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/diplom/agronomistadviser/AgronomistAdviserApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.diplom.agronomistadviser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgronomistAdviserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
